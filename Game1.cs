2a56035daecea385802f54fd42250c86f69d4de9
Chris Stone <contact@chrisstone.dev>
2021-12-08 05:13:08 -0500

    Completed original tutorial

--- Game1.cs
+++ Game1.cs
@@ -19,9 +19,11 @@ namespace MonoGameWorkshop
         private Body playerBody;
         private Vector2 playerPosition = new(width * 0.5f, height * 0.3f);
         private Vector2 playerSize = new(width * 0.06f, height * 0.12f);
-
         private const int width = 200;
         private const int height = 120;
+        private bool movingLeft = true;
+        private Vector2 platformPosition = new Vector2(width * 0.5f, height * 0.85f);
+        private Vector2 platformSize = new Vector2(width * 0.25f, height * 0.10f);
 
         public Game1()
         {
@@ -51,6 +53,10 @@ namespace MonoGameWorkshop
             var leftEdge = world.CreateEdge(new Vector2(0, 0), new Vector2(0, height));
             var rightEdge = world.CreateEdge(new Vector2(width, 0), new Vector2(width, height));
             var topEdge = world.CreateEdge(new Vector2(0, 0), new Vector2(width, 0));
+            var platformBody = world.CreateBody(platformPosition, 0, BodyType.Static);
+            var platformFixture = platformBody.CreateRectangle(platformSize.X, platformSize.Y, 0, Vector2.Zero);
+            platformFixture.Restitution = 0.1f;
+            platformFixture.Friction = 0.5f;
 
             base.Initialize();
         }
@@ -71,6 +77,28 @@ namespace MonoGameWorkshop
 
             world.Step((float)gameTime.ElapsedGameTime.TotalSeconds);
 
+            playerBody.Rotation = 0;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                movingLeft = false;
+                playerBody.ApplyLinearImpulse(new Vector2(800, 0));
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                movingLeft = true;
+                playerBody.ApplyLinearImpulse(new Vector2(-800, 0));
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                if (Math.Abs(playerBody.LinearVelocity.Y) < 1)
+                {
+                    playerBody.LinearVelocity = new Vector2(playerBody.LinearVelocity.X, -200);
+                }
+            }
+
             base.Update(gameTime);
         }
 
@@ -83,8 +111,14 @@ namespace MonoGameWorkshop
             spriteBatch.End();
 
             spriteBatch.Begin(transformMatrix: camera.GetViewMatrix());
+
             var playerDestination = new Rectangle((int)(playerBody.Position.X - playerSize.X / 2), (int)(playerBody.Position.Y - (playerSize.Y / 2)), (int)playerSize.X, (int)playerSize.Y);
-            spriteBatch.Draw(playerTexture, playerDestination, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0);
+            var effect = movingLeft ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+            spriteBatch.Draw(playerTexture, playerDestination, null, Color.White, 0, Vector2.Zero, effect, 0);
+
+            var platformDestination = new Rectangle((int)(platformPosition.X - platformSize.X / 2), (int)(platformPosition.Y - platformSize.Y / 2), (int)platformSize.X, (int)platformSize.Y);
+            spriteBatch.Draw(platformTexture, platformDestination, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0);
+
             spriteBatch.End();
 
             base.Draw(gameTime);
